eccc07c431ccc32eb821f4829128eff85acfffde
cleland32base <cleland32base@yahoo.com>
2021-08-25 00:11:59 -0500

    Updates

---
+++ precache-manifest.f511127fed7dfc69b91384caa53ceb50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef2cc5f1e09790db487f1dc013e3b80",
+    "url": "/Cleland-Resume/index.html"
+  },
+  {
+    "revision": "05d886623c0095976876",
+    "url": "/Cleland-Resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/Cleland-Resume/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Cleland-Resume/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d886623c0095976876",
+    "url": "/Cleland-Resume/static/js/main.e9e4f1a5.chunk.js"
+  },
+  {
+    "revision": "19ac06832108aeb392ae",
+    "url": "/Cleland-Resume/static/js/runtime-main.165171be.js"
+  }
+]);
